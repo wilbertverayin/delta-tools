a573a334bd5d3b4bb09d778724c8f4cbb7547d02
Wilbert Verayin <wilbertverayin@gmail.com>
2024-02-18 09:24:51 +0800

    fix: public path

--- assets/loading_bar.961c80da.js
+++
@@ -1,1 +0,0 @@
-import{aw as a}from"./index.0014ed2d.js";a.setDefaults({hijackFilter(){return!1}});
---
+++ assets/loading_bar.f7608e6b.js
@@ -0,0 +1,1 @@
+import{aw as a}from"./index.111a31af.js";a.setDefaults({hijackFilter(){return!1}});
